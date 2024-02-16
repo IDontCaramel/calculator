30e2def4c7cb282d35304ee4d355dbedcf17d0dd
IDontCaramel <120132450+IDontCaramel@users.noreply.github.com>
2024-02-16 11:55:59 +0100

    calculator

---
+++ Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace desktop_app_test
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculator));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.equals = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.row4 = new System.Windows.Forms.Label();
+            this.row3 = new System.Windows.Forms.Label();
+            this.row2 = new System.Windows.Forms.Label();
+            this.row1 = new System.Windows.Forms.Label();
+            this.times = new System.Windows.Forms.Button();
+            this.devide = new System.Windows.Forms.Button();
+            this.min = new System.Windows.Forms.Button();
+            this.coma = new System.Windows.Forms.Button();
+            this.backspace = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.CE = new System.Windows.Forms.Button();
+            this.MR = new System.Windows.Forms.Button();
+            this.Mplus = new System.Windows.Forms.Button();
+            this.Mmin = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.wortel = new System.Windows.Forms.Button();
+            this.procent = new System.Windows.Forms.Button();
+            this.tableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.button9, "button9");
+            this.button9.Name = "button9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button0
+            // 
+            this.button0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button0.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.button0, "button0");
+            this.button0.Name = "button0";
+            this.button0.UseVisualStyleBackColor = false;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // equals
+            // 
+            this.equals.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.equals, "equals");
+            this.equals.Name = "equals";
+            this.equals.UseVisualStyleBackColor = false;
+            this.equals.Click += new System.EventHandler(this.equals_Click);
+            // 
+            // add
+            // 
+            this.add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.add, "add");
+            this.add.Name = "add";
+            this.add.UseVisualStyleBackColor = false;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.tableLayoutPanel, "tableLayoutPanel");
+            this.tableLayoutPanel.Controls.Add(this.row4, 0, 3);
+            this.tableLayoutPanel.Controls.Add(this.row3, 0, 2);
+            this.tableLayoutPanel.Controls.Add(this.row2, 0, 1);
+            this.tableLayoutPanel.Controls.Add(this.row1, 0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            // 
+            // row4
+            // 
+            resources.ApplyResources(this.row4, "row4");
+            this.row4.Name = "row4";
+            // 
+            // row3
+            // 
+            resources.ApplyResources(this.row3, "row3");
+            this.row3.Name = "row3";
+            // 
+            // row2
+            // 
+            resources.ApplyResources(this.row2, "row2");
+            this.row2.Name = "row2";
+            this.row2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // row1
+            // 
+            resources.ApplyResources(this.row1, "row1");
+            this.row1.Name = "row1";
+            // 
+            // times
+            // 
+            this.times.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.times, "times");
+            this.times.Name = "times";
+            this.times.UseVisualStyleBackColor = false;
+            this.times.Click += new System.EventHandler(this.times_Click);
+            // 
+            // devide
+            // 
+            this.devide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.devide, "devide");
+            this.devide.Name = "devide";
+            this.devide.UseVisualStyleBackColor = false;
+            this.devide.Click += new System.EventHandler(this.devide_Click);
+            // 
+            // min
+            // 
+            this.min.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.min, "min");
+            this.min.Name = "min";
+            this.min.UseVisualStyleBackColor = false;
+            this.min.Click += new System.EventHandler(this.min_Click);
+            // 
+            // coma
+            // 
+            this.coma.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.coma, "coma");
+            this.coma.Name = "coma";
+            this.coma.UseVisualStyleBackColor = false;
+            this.coma.Click += new System.EventHandler(this.coma_Click);
+            // 
+            // backspace
+            // 
+            this.backspace.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.backspace.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.backspace, "backspace");
+            this.backspace.Name = "backspace";
+            this.backspace.UseVisualStyleBackColor = false;
+            this.backspace.Click += new System.EventHandler(this.backspace_Click);
+            // 
+            // C
+            // 
+            this.C.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.C, "C");
+            this.C.Name = "C";
+            this.C.UseVisualStyleBackColor = false;
+            this.C.Click += new System.EventHandler(this.C_Click);
+            // 
+            // CE
+            // 
+            this.CE.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.CE, "CE");
+            this.CE.Name = "CE";
+            this.CE.UseVisualStyleBackColor = false;
+            this.CE.Click += new System.EventHandler(this.CE_Click);
+            // 
+            // MR
+            // 
+            this.MR.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.MR, "MR");
+            this.MR.Name = "MR";
+            this.MR.UseVisualStyleBackColor = false;
+            this.MR.Click += new System.EventHandler(this.MR_Click);
+            // 
+            // Mplus
+            // 
+            this.Mplus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Mplus.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.Mplus, "Mplus");
+            this.Mplus.Name = "Mplus";
+            this.Mplus.UseVisualStyleBackColor = false;
+            this.Mplus.Click += new System.EventHandler(this.Mplus_Click);
+            // 
+            // Mmin
+            // 
+            this.Mmin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.Mmin, "Mmin");
+            this.Mmin.Name = "Mmin";
+            this.Mmin.UseVisualStyleBackColor = false;
+            this.Mmin.Click += new System.EventHandler(this.Mmin_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // wortel
+            // 
+            this.wortel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.wortel, "wortel");
+            this.wortel.Name = "wortel";
+            this.wortel.UseVisualStyleBackColor = false;
+            this.wortel.Click += new System.EventHandler(this.wortel_Click);
+            // 
+            // procent
+            // 
+            this.procent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.procent, "procent");
+            this.procent.Name = "procent";
+            this.procent.UseVisualStyleBackColor = false;
+            this.procent.Click += new System.EventHandler(this.procent_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            resources.ApplyResources(this, "$this");
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Controls.Add(this.procent);
+            this.Controls.Add(this.wortel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MR);
+            this.Controls.Add(this.Mplus);
+            this.Controls.Add(this.Mmin);
+            this.Controls.Add(this.CE);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.backspace);
+            this.Controls.Add(this.coma);
+            this.Controls.Add(this.min);
+            this.Controls.Add(this.devide);
+            this.Controls.Add(this.times);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.equals);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Name = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button equals;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.Label row2;
+        private System.Windows.Forms.Label row1;
+        private System.Windows.Forms.Label row4;
+        private System.Windows.Forms.Label row3;
+        private System.Windows.Forms.Button times;
+        private System.Windows.Forms.Button devide;
+        private System.Windows.Forms.Button min;
+        private System.Windows.Forms.Button coma;
+        private System.Windows.Forms.Button backspace;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button CE;
+        private System.Windows.Forms.Button MR;
+        private System.Windows.Forms.Button Mplus;
+        private System.Windows.Forms.Button Mmin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button wortel;
+        private System.Windows.Forms.Button procent;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desktop_app_test
+{
+    public partial class Calculator : Form
+    {
+
+        public Calculator()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        string som = "";
+
+        bool SwitchToNum2 = false;
+        bool calculated = false;
+
+        float memory = 0;
+
+        string prev_operator = "";
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            updatelabel("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            updatelabel("2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            updatelabel("3");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            updatelabel("4");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            updatelabel("5");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            updatelabel("6");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            updatelabel("7");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            updatelabel("8");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            updatelabel("9");
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            updatelabel("0");
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            operate("+");
+        }
+
+        private void min_Click(object sender, EventArgs e)
+        {
+            operate("-");
+        }
+
+        private void devide_Click(object sender, EventArgs e)
+        {
+            operate("/");
+        }
+
+        private void times_Click(object sender, EventArgs e)
+        {
+            operate("x");
+        }
+
+        private void procent_Click(object sender, EventArgs e)
+        {
+            operate("%");
+        }
+
+        private void wortel_Click(object sender, EventArgs e)
+        {
+            operate("√");
+        }
+
+
+
+        public void operate(string operator_used)
+        {
+            
+
+            if (row1.Text != "" && row3.Text != "")
+            {
+                row1.Text = calc(prev_operator);
+                row2.Text = "";
+                row3.Text = "";
+                row4.Text = "";
+            }
+
+
+            else if (calculated == true)
+            {
+                row1.Text = row4.Text;
+                row2.Text = "";
+                row3.Text = "";
+                row4.Text = "";
+            }
+
+            if (row1.Text != "" && row3.Text == "")
+            {
+
+                
+                if (operator_used == "√")
+                {
+                    calc_wortel(row1.Text);
+                }
+
+                else
+                {
+                    prev_operator = operator_used;
+                    row2.Text = operator_used;
+                    SwitchToNum2 = true;
+                    som = "";
+                }
+            }
+        }
+
+
+        public void updatelabel(string num)
+        {
+            if (num == "remove")
+            {
+                if (!string.IsNullOrEmpty(som) && row1.Text.Length > 0)
+                {
+                    if (SwitchToNum2 == true)
+                    {
+                        som = som.Remove(row3.Text.Length - 1, 1);
+                    }
+                    else
+                    {
+                        som = som.Remove(row1.Text.Length - 1, 1);
+                    }
+
+                }
+                else
+                {
+                    
+                }
+                
+            }
+            else
+            {
+                som = som + num;
+            }
+
+
+            if (SwitchToNum2 == false)
+            {
+                row1.Text = som;
+            }
+
+            else if (SwitchToNum2 == true)
+            {
+                row3.Text = som;
+            }
+            
+        }
+
+
+
+        public string calc(string operator_used)
+        {
+            float.TryParse(row1.Text, out float num1);
+            float.TryParse(row3.Text, out float num2);
+
+            if (row1.Text != "" && row3.Text != "")
+            {
+                calculated = true;
+
+                if (operator_used == "+")
+                {
+                    return (num1 + num2).ToString();
+                }
+                else if (operator_used == "-")
+                {
+                    return (num1 - num2).ToString();
+                }
+                else if (operator_used == "x")
+                {
+                    return (num1 * num2).ToString();
+                }
+                else if (operator_used == "/")
+                {
+                    return (num1 / num2).ToString();
+                }
+                else if (operator_used == "%")
+                {
+                    return (num1 / 100 * num2).ToString();
+                }
+                else
+                {
+                    return row1.Text;
+                }
+
+            }
+            else
+            {
+                return row1.Text;
+            }
+            SwitchToNum2 = false;
+            calculated = false;
+
+        }
+
+        private void equals_Click(object sender, EventArgs e)
+        {
+            row4.Text = calc(prev_operator);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void coma_Click(object sender, EventArgs e)
+        {   
+            updatelabel(",".ToString());
+        }
+
+        private void backspace_Click(object sender, EventArgs e)
+        {
+            updatelabel("remove");
+        }
+
+        private void CE_Click(object sender, EventArgs e)
+        {
+            som = "";
+            row3.Text = som;
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+            som = "";
+            row1.Text = som;
+            row2.Text = som;
+            row3.Text = som;
+            row4.Text = som;
+            SwitchToNum2 = false;
+            calculated = false;
+        }
+
+        int count = 1;
+        private void MR_Click(object sender, EventArgs e)
+        {
+            row1.Text = memory.ToString();
+            count++;
+
+            if (count >= 2)
+            {
+                memory = 0;
+                count = 1;
+            }
+        }
+
+        private void Mplus_Click(object sender, EventArgs e)
+        {
+            float.TryParse(row4.Text, out float memadd);
+
+            float MemValue = memory + memadd;
+
+            if (memadd != 0)
+            {
+                row4.Text = MemValue.ToString();
+                memory = memory + memadd;
+            }
+            
+
+        }
+
+        private void Mmin_Click(object sender, EventArgs e)
+        {
+            float.TryParse(row4.Text, out float memmin);
+
+            float MemValue = memory - memmin;
+
+            if (memmin != 0)
+            {
+                row4.Text = MemValue.ToString();
+                memory = memory - memmin;
+            }
+            
+        }
+
+        
+        public void calc_wortel(string num_str)
+        {
+            float.TryParse(num_str, out float num);
+
+            row4.Text =  Math.Sqrt(num).ToString();
+        }
+
+        
+    }
+}
